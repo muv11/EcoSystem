9ddc7ade503a045b456f212723c5739a9a744633
muv11 <uvmozgovaya@proton.me>
2023-08-15 18:59:33 +0500

    Initial commit

---
+++ src/main/java/com/muv/ecosystem/EcoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.muv.ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muv/ecosystem/EcoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muv.ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
