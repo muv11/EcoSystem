40f093fc3633e2393c6bd95dc4256f456190022b
muv11 <uliamozgovaya03@gmail.com>
2023-08-18 17:14:16 +0500

    Add enableJpaRepositories annotation

--- src/main/java/com/muv/ecosystem/config/ProjectConfig.java
+++ src/main/java/com/muv/ecosystem/config/ProjectConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -17,6 +18,7 @@ import java.util.Objects;
 @Configuration
 @ComponentScan(basePackages = "java")
 @PropertySource("classpath:application.properties")
+@EnableJpaRepositories(basePackages = "com.muv.ecosystem.repository")
 public class ProjectConfig {
 
     private final Environment environment;
