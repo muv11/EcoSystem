eb807759435922118fbe40d3ed185e5798b3a170
muv11 <uvmozgovaya@proton.me>
2023-08-19 18:16:16 +0500

    Move map code to map.js, add functions for modal windows' opening and closing

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,76 @@
-const apiKey = '6bf67d2e-d7e9-4093-b884-b776818f9a44';
-const centerCoordinates = [60.597474, 56.838011]; //lon, lat
+const loginBlock = document.getElementById('login');
+const firstRegBlock = document.getElementById('reg_first');
+const secondRegBlock = document.getElementById('reg_second');
+const overlay = document.getElementById('overlay');
 
-const map = new mapgl.Map('map_container', {
-    key: apiKey,
-    center: centerCoordinates,
-    zoom: 10,
-});
+function openLoginWindow() {
+    document.getElementById('p_log_in').addEventListener('click', function () {
+       loginBlock.style.display = 'block';
+       overlay.style.pointerEvents = 'auto';
+    });
+}
+
+function openFirstRegistrationWindow() {
+    document.getElementById('p_register').addEventListener('click', function () {
+        firstRegBlock.style.display = 'block';
+        overlay.style.pointerEvents = 'auto';
+    });
+}
+
+function openSecondRegistrationWindow() {
+    document.getElementById('reg_first_btn').addEventListener('click', function () {
+        secondRegBlock.style.display = 'block';
+        overlay.style.pointerEvents = 'auto';
+    });
+}
+
+function openModalWindows() {
+    openLoginWindow();
+    openFirstRegistrationWindow();
+    openSecondRegistrationWindow();
+}
+
+function closeLoginModalWindow() {
+    overlay.addEventListener('click', function (event) {
+        let isBlockOuter = event.target === overlay;
+        if (isBlockOuter) {
+            if (loginBlock.style.display !== 'none') {
+                loginBlock.style.display = 'none';
+                overlay.style.pointerEvents = 'none';
+            }
+        }
+    });
+}
+
+function closeFirstRegModalWindow() {
+    overlay.addEventListener('click', function (event) {
+        let isBlockOuter = event.target === overlay;
+        if (isBlockOuter) {
+            if (firstRegBlock.style.display !== 'none') {
+                firstRegBlock.style.display = 'none';
+                overlay.style.pointerEvents = 'none';
+            }
+        }
+    });
+}
+
+function closeSecondRegModalWindow() {
+    overlay.addEventListener('click', function (event) {
+        let isBlockOuter = event.target === overlay;
+        if (isBlockOuter) {
+            if (secondRegBlock.style.display !== 'none') {
+                secondRegBlock.style.display = 'none';
+                overlay.style.pointerEvents = 'none';
+            }
+        }
+    });
+}
+
+function closeModalWindows() {
+    closeLoginModalWindow();
+    closeFirstRegModalWindow();
+    closeSecondRegModalWindow();
+}
+
+openModalWindows();
+closeModalWindows();
