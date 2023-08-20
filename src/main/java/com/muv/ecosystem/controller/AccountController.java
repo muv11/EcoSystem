91268d63643fe8c88a1895c2b9c67758be82f465
muv11 <uliamozgovaya03@gmail.com>
2023-08-20 13:14:08 +0500

    Change getMainPage method: it returns different pages according to user's role

--- src/main/java/com/muv/ecosystem/controller/AccountController.java
+++ src/main/java/com/muv/ecosystem/controller/AccountController.java
@@ -1,14 +1,32 @@
 package com.muv.ecosystem.controller;
 
+import com.muv.ecosystem.entity.Role;
+import com.muv.ecosystem.service.UserManagementService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
 
+    private final UserManagementService userManagementService;
+
+    public AccountController(UserManagementService userManagementService) {
+        this.userManagementService = userManagementService;
+    }
+
     @GetMapping("/account")
     public String getMainPage() {
-        return "user_account";
+        Role role = userManagementService.getRole();
+        if (role.getName().equals("user")) {
+            return "user_account";
+        }
+        if (role.getName().equals("eco_specialist")) {
+            return "eco_specialist_account";
+        }
+        if (role.getName().equals("content_manager")) {
+            return "content_manager_account";
+        }
+        return "index";
     }
 
 }
