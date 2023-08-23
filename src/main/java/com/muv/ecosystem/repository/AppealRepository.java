0b28b81cb5a0ee9be07890a60ae94ce30fed4529
muv11 <uliamozgovaya03@gmail.com>
2023-08-23 18:22:13 +0500

    Create repository for Appeal entity that extends JpaRepository

---
+++ src/main/java/com/muv/ecosystem/repository/AppealRepository.java
@@ -0,0 +1,7 @@
+package com.muv.ecosystem.repository;
+
+import com.muv.ecosystem.entity.Appeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppealRepository extends JpaRepository<Appeal, Integer> {
+}
