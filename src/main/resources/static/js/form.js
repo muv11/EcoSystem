1add8b4ebd3fa033ab3abe755dd8d5f99d9f6723
muv11 <uliamozgovaya03@gmail.com>
2023-08-23 15:33:28 +0500

    Split validateLoginForm method into two

--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -1,28 +1,36 @@
-const loginMsg = document.getElementById('lgn_login_empty_msg');
-const passwordMsg = document.getElementById('lgn_password_empty_msg');
-
-function validateLoginFormInput() {
+function validateLoginInLoginForm() {
     const submitButton = document.getElementById('login_form_submit');
+    const loginMsg = document.getElementById('lgn_login_empty_msg');
     const loginInput = document.getElementById('lgn_login_input');
     loginMsg.style.display = 'none';
+    submitButton.disabled = true;
     loginInput.addEventListener('input', function () {
         if (loginInput.value === null || loginInput.value === '') {
             loginMsg.style.display = 'block';
+            document.getElementById('br_login_1').style.display = 'none';
             submitButton.disabled = true;
         } else {
             loginMsg.style.display = 'none';
+            document.getElementById('br_login_1').style.display = 'block';
             submitButton.disabled = false;
         }
     });
+}
 
+function validatePasswordInLoginForm() {
+    const submitButton = document.getElementById('login_form_submit');
+    const passwordMsg = document.getElementById('lgn_password_empty_msg');
     const passwordInput = document.getElementById('lgn_password_input');
     passwordMsg.style.display = 'none';
+    submitButton.disabled = true;
     passwordInput.addEventListener('input', function () {
         if (passwordInput.value === null || passwordInput.value === '') {
             passwordMsg.style.display = 'block';
+            document.getElementById('br_login_2').style.display = 'none';
             submitButton.disabled = true;
         } else {
             passwordMsg.style.display = 'none';
+            document.getElementById('br_login_2').style.display = 'block';
             submitButton.disabled = false;
         }
     });
