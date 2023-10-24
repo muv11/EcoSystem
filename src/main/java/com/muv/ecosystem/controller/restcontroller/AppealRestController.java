c447556fdf03d3fe0bc2994b8e2937a44c8ca33e
muv11 <uliamozgovaya03@gmail.com>
2023-10-24 12:23:09 +0500

    Add method that gets appeals by current user

--- src/main/java/com/muv/ecosystem/controller/restcontroller/AppealRestController.java
+++ src/main/java/com/muv/ecosystem/controller/restcontroller/AppealRestController.java
@@ -2,6 +2,7 @@ package com.muv.ecosystem.controller.restcontroller;
 
 import com.muv.ecosystem.entity.Appeal;
 import com.muv.ecosystem.repository.AppealRepository;
+import com.muv.ecosystem.service.AppealService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,21 @@ import java.util.List;
 public class AppealRestController {
 
     private final AppealRepository appealRepository;
+    private final AppealService appealService;
 
-    public AppealRestController(AppealRepository appealRepository) {
+    public AppealRestController(AppealRepository appealRepository, AppealService appealService) {
         this.appealRepository = appealRepository;
+        this.appealService = appealService;
     }
 
     @GetMapping
     public List<Appeal> getAppeals() {
         return appealRepository.findAll();
+    }
+
+    @GetMapping("/current-user")
+    public List<Appeal> getAppealsByCurrentUser() {
+        return appealService.getAppealsByCurrentUser();
     }
 
 }
